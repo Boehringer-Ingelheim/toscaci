476f532c0e814970565e822172dc556b45a4bb76
Isaac Aymerich <isaac.aymerich@gmail.com>
2021-06-23 18:07:05 +0200

    Fixes (#7)
    Fix #7 delimiting interrupt type signals only

--- client/src/go/toscactl/main.go
+++ client/src/go/toscactl/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
@@ -10,13 +11,14 @@ import (
 )
 
 func main() {
-	signalchan := make (chan os.Signal)
-	signal.Notify(signalchan)
+	signalchan := make (chan os.Signal,1)
+	signal.Notify(signalchan,os.Interrupt,os.Kill)
 	ctx, stop :=context.WithCancel(context.Background())
 	defer stop()
 	go func() {
 		select {
 		case <-signalchan:
+			fmt.Println()
 			log.Info("Program interruption detected... closing...")
 			stop()
 		}
